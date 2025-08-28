7224fb7033789cc5d6f01e362f8110abfc7601d2
GooseMetrics <joyce@goosemetrics.com>
2025-08-27 22:14:00 -0300

    code added in app.s to always jump to top after navigation

--- app.js
+++ app.js
@@ -5,8 +5,14 @@
             this.classList.add("active-btn");
             document.querySelector(".active").classList.remove("active");
             document.getElementById(button.dataset.id).classList.add("active");
+
+            // ADDED: always jump to top after navigation
+            document.documentElement.scrollTop = 0;
+            document.body.scrollTop = 0;
+            window.scrollTo({ top: 0, left: 0, behavior: "auto" });
         })
     });
+
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
     })
